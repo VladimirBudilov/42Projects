1e51b342e5b1a3912e067462ec831bdfae1fd866
VladimirBudilov <109314834+VladimirBudilov@users.noreply.github.com>
2023-04-05 19:24:16 +0400

    added minitalk

---
+++ Minitalk/server.c
@@ -0,0 +1,46 @@
+#include <signal.h>
+#include <unistd.h>
+#include "Libft/libft.h"
+
+static void	action(int sig, siginfo_t *info, void *context)
+{
+	static int				i = 0;
+	static pid_t			client_pid = 0;
+	static unsigned char	c = 0;
+
+	(void)context;
+	if (!client_pid)
+		client_pid = info->si_pid;
+	c |= (sig == SIGUSR2);
+	if (++i == 8)
+	{
+		i = 0;
+		if (!c)
+		{
+			kill(client_pid, SIGUSR2);
+			client_pid = 0;
+			return ;
+		}
+		ft_putchar_fd(c, 1);
+		c = 0;
+		kill(client_pid, SIGUSR1);
+	}
+	else
+		c <<= 1;
+}
+
+int	main(void)
+{
+	struct sigaction	s_sigaction;
+
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
+	s_sigaction.sa_sigaction = action;
+	s_sigaction.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &s_sigaction, 0);
+	sigaction(SIGUSR2, &s_sigaction, 0);
+	while (1)
+		pause();
+	return (0);
+}x
