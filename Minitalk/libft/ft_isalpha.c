bfd276da23623c31cf1ae157b224179ff32ae2ed
VladimirBudilov <v.ladimirbudilov@yandex.ru>
2023-04-18 12:38:08 +0400

    fixed isalpha and Makefile

--- Minitalk/libft/ft_isalpha.c
+++ Minitalk/libft/ft_isalpha.c
@@ -12,9 +12,9 @@
 
 int	ft_isalpha(int argument)
 {
-	if (argument >= 'a' & argument <= 'z')
+	if (argument >= 'a' && argument <= 'z')
 		return (1);
-	else if (argument >= 'A' & argument <= 'Z')
+	else if (argument >= 'A' && argument <= 'Z')
 		return (1);
 	else
 		return (0);
